f9e94dde87f6acbea09ceecad5891795f80e5295
patkainer <kainer.patrick@gmail.com>
2021-10-09 08:23:21 +0200

    added DemoArithetischeOperationen changed Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,9 +3,9 @@ import java.sql.SQLOutput;
 public class Calculator {
 
     public static void main(String[] args) {
-        int a= 1;
+        int a= 500;
         System.out.println(a);
-        double b = 2.3;
+        int b = 600;
         System.out.println(b);
         boolean c = true;
         System.out.println(c);
@@ -13,10 +13,43 @@ public class Calculator {
         System.out.println(d);
         String e = "Bier";
         System.out.println(e);
+        out(add(a,b));
+        out(sub(a,b));
+        out(mul(a,b));
+        out(div(a,b));
+        out(mod(a,b));
 
 
 
     }
+    public static int add(int a, int b )
+    {
 
+        return a + b;
+    }
+    public static int sub(int a, int b )
+    {
+
+        return a - b;
+    }
+    public static int div(int a, int b )
+    {
+
+        return a / b;
+    }
+    public static int mul(int a, int b )
+    {
+
+        return a * b;
+    }
+    public static int mod(int a, int b )
+    {
+
+        return a % b;
+    }
+    public static void out(int n)
+    {
+        System.out.println(n);
+    }
 }
 
---
+++ src/DemoArithetischeOperationen.java
@@ -0,0 +1,17 @@
+public class DemoArithetischeOperationen {
+    //psvm
+    public static void main(String[] args) {
+
+        int erg;
+        erg = 27%20;
+        System.out.println("erg = " + erg);
+        System.out.println("args = " + args);
+    }
+    //initialisierung
+
+    //Opertoren
+    //+,-,*,/
+    //%== Modulo Rest von Division
+    //soutv
+
+}
